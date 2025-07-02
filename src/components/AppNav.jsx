d732fece93278f5fee3d8585d79308f0673ccc04
codejordy25 <jordytsimba11@gmail.com>
2025-07-02 10:41:24 +0100

    Set up App navigation

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,4 +1,7 @@
 import styles from "./AppNav.module.css";
+// import {nav} from "./AppNav.module.css";
+//className={nav} C'est Pour Lier un objet à un style.
+
 function AppNav() {
   //styles.nav C'est Pour Lier un objet à un style.
   return <nav className={styles.nav}>App navigation</nav>;
