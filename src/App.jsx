3760b1ace21b5d6a9ea719ba3f1a18cf4ab7222b
codejordy25 <jordytsimba11@gmail.com>
2025-07-06 20:49:43 +0100

    Import Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
             <Route path="cities" element={<p>List of cities</p>} />
-            <Route path="countries" element={<p>List of countries</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
--- src/components/Sidbar.jsx
+++ src/components/Sidbar.jsx
@@ -1,3 +1,5 @@
+// 
+import { Outlet } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
