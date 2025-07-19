6f907952fef56bc0c1885733a4ec75fe39137afe
codejordy25 <jordytsimba11@gmail.com>
2025-07-19 14:08:43 +0100

    transfert l'etat dans URL, R/set a query String

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,21 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searhParams, setSearchParams] = useSearchParams();
+  const lat = searhParams.get("lat");
+  const lng = searhParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1> Map</h1>
+      <h1>
+        position: {lat}, {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 25, lng: 30 })}>
+        change pos
+      </button>
+    </div>
+  );
 }
 
 export default Map;
